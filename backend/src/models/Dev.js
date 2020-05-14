956a80402961fc6aada584811ad38b15d6a2a3e5
Alex Botelho <alexbotelho1@hotmail.com>
2020-05-14 18:46:24 -0300

    .

--- backend/src/models/Dev.js
+++ backend/src/models/Dev.js
@@ -18,7 +18,10 @@ const DevSchema = new Schema({
        type: Schema.Types.ObjectId,
        ref: 'Dev',
    }],
-   deslikes: []
+   dislikes: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Dev',
+}]
 }, {
    timestamps: true,
 })
